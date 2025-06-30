865fe6590e4c4327420ab54f9043f96dad12f06a
Blazzzeee <legendgamer1007@gmail.com>
2025-06-30 12:22:04 +0530

    fixed lsp references

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,96 +1,90 @@
 return {
-
     -- Mason
     {
         "williamboman/mason.nvim",
-          cmd = "Mason",               -- load on :Mason
-         event = "BufReadPre",          config = function()
+        cmd = "Mason",
+        event = "BufReadPre",
+        config = function()
             require("mason").setup()
-            opts = {
-                ensure_installed = {
-                    "clangd",
-                    "clangformat",
-                    "codelldb",
-                },
+            -- Optional non-LSP tools (already installed here)
+            local mason_registry = require("mason-registry")
+            local ensure_installed = {
+                "clangd",
             }
+            for _, tool in ipairs(ensure_installed) do
+                local p = mason_registry.get_package(tool)
+                if not p:is_installed() then
+                    p:install()
+                end
+            end
         end,
     },
-    -- Mason LSPconfig
+
+    -- Mason LSPconfig + all LSP setup logic
     {
-        "williamboman/mason-lspconfig.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "williamboman/mason.nvim" },
+        "mason-org/mason-lspconfig.nvim",
+        lazy = true,
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "pyright", "lua_ls" },
+                ensure_installed = { "pyright", "lua_ls", "clangd" },
             })
-        end,
+        end
     },
-    -- LSPconfig
     {
         "neovim/nvim-lspconfig",
         event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "williamboman/mason-lspconfig.nvim" },
+        dependencies = { "hrsh7th/cmp-nvim-lsp", "mason-org/mason-lspconfig.nvim" },
         config = function()
             local lspconfig = require("lspconfig")
-            local servers = { "pyright", "lua_ls", "ts_ls", "clangd" }
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            local telescope = require("telescope.builtin")
+            local servers = { "clangd", "lua_ls", "pyright" }
+
+            local on_attach = function(_, _)
+                vim.keymap.set("n", "<leader>n", vim.lsp.buf.rename, { desc = "Rename symbol" })
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+                vim.keymap.set("n", "gi", telescope.lsp_implementations, { desc = "Goto implementation" })
+                vim.keymap.set("n", "gr", telescope.lsp_references, { desc = "Goto references" })
+                vim.keymap.set("n", "gk", vim.lsp.buf.hover, { desc = "Symbol info" })
+                vim.keymap.set("n", "<leader>q", function()
+                    vim.diagnostic.setqflist()
+                end, { desc = "Diagnostics to quickfix list" })
+            end
+
+            for _, server in ipairs(servers) do
+                vim.lsp.config(server, {
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                    settings = {},
+                })
+            end
+
+            -- Manual setup for Emmet LSP (not managed by Mason)
             lspconfig.emmet_language_server.setup({
                 filetypes = {
-                    "css",
-                    "eruby",
-                    "html",
-                    "javascript",
-                    "javascriptreact",
-                    "less",
-                    "sass",
-                    "scss",
-                    "pug",
-                    "typescriptreact",
+                    "css", "eruby", "html", "javascript", "javascriptreact",
+                    "less", "sass", "scss", "pug", "typescriptreact",
                 },
-                -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-                -- **Note:** only the options listed in the table are supported.
                 init_options = {
-                    ---@type table<string, string>
                     includeLanguages = {},
-                    --- @type string[]
                     excludeLanguages = {},
-                    --- @type string[]
                     extensionsPath = {},
-                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
                     preferences = {},
-                    --- @type boolean Defaults to `true`
                     showAbbreviationSuggestions = true,
-                    --- @type "always" | "never" Defaults to `"always"`
                     showExpandedAbbreviation = "always",
-                    --- @type boolean Defaults to `false`
                     showSuggestionsAsSnippets = false,
-                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
                     syntaxProfiles = {},
-                    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
                     variables = {},
                 },
             })
-            local on_attach = function(_, _)
-                vim.keymap.set("n", "<leader>n", vim.lsp.buf.rename, {desc = "Rename symbol"})
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {desc = "Code actions"})
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto defination"})
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Goto implementation"})
-                vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "Goto reference "})
-                vim.keymap.set("n", "gk", vim.lsp.buf.hover, {desc = "Symbol definition (LSP)"})
-
-                vim.keymap.set("n", "<leader>q", function()
-                    vim.diagnostic.setqflist()
-                end, { desc = "Add diagnostics to quickfix list" })
-            end
-
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            for _, server in ipairs(servers) do
-                lspconfig[server].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end
         end,
     },
-    -- Additional LSP plugins can be added here
 }
