84bc2522f6e1b8417565685ed9aa257c64d897d2
Blazzzeee <legendgamer1007@gmail.com>
2025-08-29 14:36:36 +0530

    based config

---
+++ init.lua
@@ -0,0 +1,60 @@
+local vim = vim
+
+vim.opt.winborder = "rounded"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.termguicolors = true
+vim.opt.ignorecase=true
+vim.opt.smartindent=true
+vim.opt.incsearch = true
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>r", ":update<CR> :source<CR>")
+
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+
+})
+
+require "mini.pick".setup()
+require "oil".setup()
+require "nvim-treesitter.configs".setup({
+				ensure_installed = { "lua", "javascript" , "typescript" , "python" , "c"},
+				highlight = {enable = true}
+})
+
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n" , "<leader>f", ":Pick files<CR>")
+vim.keymap.set("n" , "<leader>/", ":Pick grep_live<CR>")
+vim.keymap.set("n" , "<leader>h", ":Pick help<CR>")
+vim.keymap.set("n" , "<leader>e", ":Oil<CR>")
+
+vim.keymap.set({"n" , "v" , "x"} , "<leader>y", '"+y')
+vim.keymap.set({"n" , "v" , "x" } , "<leader>d", '"+d<CR>')
+
+-- completions
+-- vim.api.nvim_create_autocmd('LspAttach', {
+-- 	callback = function (ev)
+-- 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+-- 		if client:supports_method('textDocument/completion') then
+-- 			vim.lsp.completion.enable(true , client.id , ev.buf , { autotrigger = true})
+-- 		end
+-- 	end,
+-- })
+--
+-- vim.cmd("set completeopt+=noselect")
+
+
+vim.cmd("colorscheme vague")
+-- vim.cmd("hi statusline guibg=NONE")
+
+vim.lsp.enable({ "lua_ls", "pyright" , "clangd" })
