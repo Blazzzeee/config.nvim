4ab52d119a4f7bd3101b3b501122629cbceffeae
Blazzzeee <maaniukhurana1007@gmail.com>
2025-10-28 20:41:06 +0530

    better based config

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 local vim = vim
 
--- vim.opt.winborder = "rounded"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
@@ -12,6 +11,7 @@ vim.opt.termguicolors = true
 vim.opt.ignorecase = true
 vim.opt.smartindent = true
 vim.opt.incsearch = true
+vim.opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = " "
 
@@ -19,32 +19,40 @@ vim.keymap.set("n", "<leader>r", ":update<CR> :source<CR>")
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
-	-- { src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/ibhagwan/fzf-lua"},
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/Saghen/blink.cmp" },
+  { src = "https://www.github.com/windwp/nvim-autopairs" },
 })
 
-require "mini.pick".setup()
--- require "oil".setup()
+require "oil".setup()
+require "fzf-lua".setup()
 require "nvim-treesitter.configs".setup({
 	ensure_installed = { "lua", "javascript", "typescript", "python", "c" },
 	highlight = { enable = true }
 })
+require("nvim-autopairs").setup({
+    check_ts = true,
+    fast_wrap = {},
+})
 
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references)
-vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
-vim.keymap.set("n", "<leader>/", ":Pick grep_live<CR>")
-vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
--- vim.keymap.set("n", "<leader>e", ":Oil<CR>")
-
-vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y')
-vim.keymap.set({ "n", "v", "x" }, "<leader>d", '"+d<CR>')
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "<leader>f", ":lua require('fzf-lua').files()<CR>")
+vim.keymap.set("n", "<leader>/", ":lua require('fzf-lua').live_grep()<CR>")
+vim.keymap.set("n", "<leader>h", ":lua require('fzf-lua').helptags()<CR>")
+vim.keymap.set("n", "<leader>s", ":lua require('fzf-lua').resume()<CR>")
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
 
 require("blink-cmp").setup({
-	keymap = { preset = "default" },
+keymap = {
+    -- preset = "default",
+    -- ["<Tab>"] = { "select_next", "fallback" },
+    -- ["<S-Tab>"] = { "select_prev", "fallback" },
+    -- ["<CR>"] = { "accept", "fallback" },
+  },
 	sources = {
 		default = { "lsp", "path", "snippets", "buffer" },
 		providers = {
@@ -79,10 +87,8 @@ require("blink-cmp").setup({
 	}
 })
 
-
 vim.cmd("colorscheme vague")
 -- vim.cmd("hi statusline guibg=NONE")
 
 vim.lsp.enable({ "lua_ls", "pyright", "clangd", "rubocop" })
 vim.opt.winborder = "none"
-
