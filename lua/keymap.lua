d320919ea04d75d67e05eb2466f6784202ba4211
Blazzzeee <maaniukhurana1007@gmail.com>
2025-12-02 20:41:56 +0530

    tidy up some code

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,4 +1,5 @@
 --Execute lua file
+vim.api.nvim_set_keymap("", "<Up>", "<Nop>", {})
     vim.keymap.del("n", "grr")
     vim.keymap.del("n", "gri")
     vim.keymap.del("n", "grn")
@@ -26,59 +27,6 @@ vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>", { desc = "Quickfix next item" })
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>", { desc = "Quickfix next item" })
 
 -- Helix-style movement mappings
-
-local wk = require("which-key")
-
-wk.add({
-	{
-		"gB",
-		"B",
-		desc = "Prev WORD",
-		noremap = true,
-		mode = "n",
-	},
-	{
-		"gW",
-		"W",
-		desc = "Next WORD",
-		noremap = true,
-		mode = "n",
-	},
-	{
-		"gb",
-		"b",
-		desc = "Prev word",
-		noremap = true,
-		mode = "n",
-	},
-})
-wk.add({
-  {
-    "<leader>wa",
-    "o-- TODO: <Esc>",
-    desc = "Add TODO",
-    mode = "n",
-    noremap = true,
-    silent = true,
-  },
-  {
-    "<leader>wq",
-    "<cmd>TodoQuickfix<CR>",
-    desc = "Add TODO to quickfix",
-    mode = "n",
-    noremap = true,
-    silent = true,
-  },
-  {
-    "<leader>w/",
-    "<cmd>TodoTelescope<CR>",
-    desc = "Search TODO",
-    mode = "n",
-    noremap = true,
-    silent = true,
-  },
-})
-
 vim.keymap.set("n", "<leader>q", "<cmd>Neotree toggle<CR>", { desc = "Open file tree" })
 
 vim.keymap.set({"n", "v", "o"}, "gh", "^", { desc = "Goto line start", noremap = true })
