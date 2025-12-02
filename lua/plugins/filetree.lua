cd77ab66efbf4261d4674da28da48101c9e0185f
Blazzzeee <maaniukhurana1007@gmail.com>
2025-12-02 20:40:56 +0530

    oil as filetree

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,43 +1,16 @@
--- return {
---   "stevearc/oil.nvim",
---   cmd = "Oil",
---   keys = {
---     { "-", function() require("oil").open() end, desc = "Open parent directory (Oil)" },
---   },
---   opts = {
---     default_file_explorer = true,
---     view_options = {
---       show_hidden = true,
---     },
---   },
---   dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional, for file icons
--- }
---
 return {
-  "mikavilpas/yazi.nvim",
-  dependencies = { "folke/snacks.nvim" },
-  keys = {
-    {
-      "<leader>-",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
-    },
-    {
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
-    },
-  },
-  opts = {
-    open_for_directories = false,
-    keymaps = {
-      show_help = "<f1>",
-    },
-  },
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    config = function()
+        require("oil").setup()
+
+        vim.keymap.set("n", "-", "<cmd>Oil<CR>", { noremap = true, desc = "Open filetree" })
+    end,
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
 }
