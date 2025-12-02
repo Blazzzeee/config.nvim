fe58e5684365eac908992442c8392443f2a6da9d
Blazzzeee <maaniukhurana1007@gmail.com>
2025-12-02 20:40:39 +0530

    add more linters to conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,32 +1,49 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>b",
-			function()
-				require("conform").format({ async = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
-	---@module "conform"
-	---@type conform.setupOpts
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			-- Add more filetypes if needed
-		},
-		default_format_opts = {
-			lsp_format = "fallback", -- Use LSP if no formatter found
-		},
-	},
-	init = function()
-		-- Set formatexpr to use conform
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+
+  keys = {
+    {
+      "<leader>b",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      -- Existing
+      lua = { "stylua" },
+      python = { "isort", "black" },
+
+      -- JavaScript / TypeScript
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+
+      -- JSX/TSX (sometimes categorized separately in configs)
+      jsx = { "prettierd", "prettier", stop_after_first = true },
+      tsx = { "prettierd", "prettier", stop_after_first = true },
+
+      -- Ruby
+      ruby = { "rubocop" }, -- OR you can use ruby-lsp, choose one
+
+      -- ERB
+      erb = { "erb_format" },
+    },
+
+    default_format_opts = {
+      lsp_format = "fallback", -- use LSP if no formatter found
+    },
+  },
+
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
