fce57015b26ea1c356e94564be9b98391a084130
Blazzzeee <maaniukhurana1007@gmail.com>
2025-12-02 20:39:57 +0530

    cleanup colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,24 +1,10 @@
 return {
-    {
-        "zenbones-theme/zenbones.nvim",
-        -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-        dependencies = "rktjmp/lush.nvim",
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        'AlexvZyl/nordic.nvim',
-        lazy = true,
-        priority = 1000,
-        config = function()
-            require('nordic').load()
-        end
-    },
 
     {
         "vague2k/vague.nvim",
         config = function()
             require("vague").setup({
+            transparent = true,
             })
         end
     },
